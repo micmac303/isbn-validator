c9d18e6103683801cac6c7bb7d82d6fbc9be9246
michaelmccarthy <micmac303@gmail.com>
2024-07-04 23:28:45 +0100

    Partial solution

--- src/main/java/com/timeless/IsbnValidator.java
+++ src/main/java/com/timeless/IsbnValidator.java
@@ -1,4 +1,24 @@
 package com.timeless;
 
 public class IsbnValidator {
+
+    public boolean validate(String isbn) {
+
+        var isbnChars = isbn.chars();
+        var numericalIsbn = isbnChars.mapToObj(c -> (char) c).filter(c -> c != '-')
+                .filter(c -> Character.isDigit(c) || c == 'X')
+                .map(c -> {
+                    if (c == 'X') return 10;
+                    else return Character.getNumericValue(c);
+                })
+                .toList();
+
+        if (numericalIsbn.size() != 10) {
+            return false;
+        }
+
+        System.out.println(numericalIsbn);
+
+        return true;
+    }
 }
--- src/test/java/com/timeless/IsbnValidatorTest.java
+++ src/test/java/com/timeless/IsbnValidatorTest.java
@@ -1,7 +1,32 @@
 package com.timeless;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IsbnValidatorTest {
 
+    private final IsbnValidator isbnValidator = new IsbnValidator();
+
+    @Test
+    void shouldReturnFalseIfNotTenCharacters() {
+
+        assertFalse(isbnValidator.validate("hello"));
+    }
+
+    @Test
+    void shouldReturnTrueIfValid() {
+
+        var validIsbn = "3598215088";
+        var validIsbnX = "359821508X";
+
+        assertTrue(isbnValidator.validate(validIsbn));
+        assertTrue(isbnValidator.validate(validIsbnX));
+    }
+
+    @Test
+    void shouldReturnFalseIfInvalid() {
+
+
+    }
 }
